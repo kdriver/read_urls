0ad011309201a49989977096a6a3ef6af70ab7e5
keith <keith@just-the-one.co.uk>
2017-07-07 11:43:03 +0000

    improved automation

--- get_them.py
+++ get_them.py
@@ -6,13 +6,19 @@ import time
 
 threads=[]
 
-with open('clean_alexa') as f:
+with open('clean_alexa_5000_log') as f:
     lines = f.readlines()
 
+log = open("log.txt","w")
+log.truncate()
+
+errors = open("errors.txt","w")
+errors.truncate()
 value=0
 
-max_threads =50
+max_threads =200
 thread_num=0
+togo=0
 
 
 def get_page(page,num):
@@ -21,23 +27,38 @@ def get_page(page,num):
 		html = response.read()
 		print (num + " :OK " + page)
 	except Exception as e:
-		print(type(e))
-		print (num + " :Oh no " +  page )
+		s = repr(e)
+		op = page + " " + s  + "\n"
+		print(op)
+		errors.write(op)
 
 for url in lines:
 	value +=1
 	if thread_num < max_threads:
 		t = threading.Thread(target=get_page, args=(url.replace('\r', '').replace('\n', ''),str(value),))
-        	threads.append(t)
+        	threads.append([t,url,str(value)])
 	        t.start()
+		togo +=1
 		thread_num += 1
 	else:
 		print ( "waiting")
 		for t in threads:
-			t.join()
+			t[0].join()
+			log.write(t[2] + ": " + t[1] )
+			togo -= 1
+			print(" left " + str(togo) + '\r' )
+		togo = 0
 		print ( "reset")
 		thread_num=0
 		threads=[]
+
+if ( togo > 0 ):
+	print ( "waiting")
+        for t in threads:
+            t[0].join()
+            log.write(t[2] + ": " + t[1] )
+            togo -= 1
+            print(" left " + str(togo) + '\r' )
 
 print("done ")
 
